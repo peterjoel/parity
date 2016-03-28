6ad224db1e1a5ee1b00840ae059cf60f71c0910a
Peter <peter.hall@algomi.com>
2016-03-28 21:09:18 +0100

    This change causes compiler panic

--- parity/price_info.rs
+++ parity/price_info.rs
@@ -18,15 +18,11 @@ impl PriceInfo {
 			.send().ok()
 			.and_then(|mut s| s.read_to_string(&mut body).ok())
 			.and_then(|_| Json::from_str(&body).ok())
-			.and_then(|json| {
-				let ethusd: f32 = if let Some(&Json::String(ref s)) = json.find_path(&["result", "ethusd"]) {
-					FromStr::from_str(&s).unwrap()
-				} else {
-					return None;
-				};
-				Some(PriceInfo {
-					ethusd: ethusd,
-				})
-			})
+			.and_then(|json| json.find_path(&["result", "ethusd"]))
+			.and_then(|&Json::String(ref s)| Some(
+				PriceInfo {
+					ethusd: FromStr::from_str(&s).unwrap(),
+				}
+			))
 	}
 }
